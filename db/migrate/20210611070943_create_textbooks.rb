a18d7b967fd387c40b525214c63bdb368eec6eab
Hempraw <78518989+Hempraw@users.noreply.github.com>
2021-06-11 16:39:02 +0900

    投稿機能の実装
    ・newアクション
    ・createアクション
    ・indexアクションにおいて一覧を表示

--- db/migrate/20210611070943_create_textbooks.rb
+++ db/migrate/20210611070943_create_textbooks.rb
@@ -1,6 +1,6 @@
-class CreateLanguages < ActiveRecord::Migration[6.0]
+class CreateTextbooks < ActiveRecord::Migration[6.0]
   def change
-    create_table :languages do |t|
+    create_table :textbooks do |t|
       t.string :name
       t.string :item
       t.text :content
